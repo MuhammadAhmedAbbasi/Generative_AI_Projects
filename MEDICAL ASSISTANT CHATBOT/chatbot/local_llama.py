41247485600f665f34440dad7c533da5ae1c8cd5
MuhammadAhmedAbbasi <engrahamadabbasi3015@gmail.com>
2024-05-16 18:20:23 +0800

    Poem and essay generator

--- MEDICAL ASSISTANT CHATBOT/chatbot/local_llama.py
+++ MEDICAL ASSISTANT CHATBOT/chatbot/local_llama.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+#os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACKING_V2"]="true"
 
